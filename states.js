7f5f2e6fa881b0055f175d66d665d4062e23d638
tylerlittlewood5-dotcom <tylerlittlewood5@gmail.com>
2026-02-22 15:41:37 -0500

    Update states.js

--- states.js
+++ states.js
@@ -1,1 +1,16 @@
+const states = [
+{ name:"California", votes:54 },
+{ name:"Texas", votes:40 },
+{ name:"Florida", votes:30 },
+{ name:"New York", votes:28 },
+{ name:"Pennsylvania", votes:19 },
+{ name:"Illinois", votes:19 },
+{ name:"Ohio", votes:17 },
+{ name:"Georgia", votes:16 },
+{ name:"Michigan", votes:15 },
+{ name:"North Carolina", votes:16 },
+{ name:"Tennessee", votes:11 },
+{ name:"Arizona", votes:11 },
+{ name:"Wisconsin", votes:10 }
+];
 
