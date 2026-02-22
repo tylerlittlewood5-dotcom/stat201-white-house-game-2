139634754cbee29b5d998c48f515c0acde93db97
tylerlittlewood5-dotcom <tylerlittlewood5@gmail.com>
2026-02-22 15:42:00 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,28 @@
+const questions = [
+
+{
+q:"A z-score represents:",
+a:["Distance from mean in SD units","Sample size","Median value","Correlation"],
+correct:0
+},
+
+{
+q:"Correlation is affected by outliers.",
+a:["True","False"],
+correct:0
+},
+
+{
+q:"A straight normal probability plot suggests:",
+a:["Non-normal","Normal distribution","Categorical data","Skewed left"],
+correct:1
+},
+
+{
+q:"Which is a bivariate quantitative display?",
+a:["Histogram","Scatterplot","Pie chart","Bar chart"],
+correct:1
+}
+
+];
 
